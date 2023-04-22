4e21194bae9d0ccbbcd34e28ae27004f9e0106ab
“MythSH20” <MythSH20.outlook.com>
2023-04-22 20:09:05 +0800

    alphav1.1

--- EVA/models.py
+++ EVA/models.py
@@ -3,10 +3,29 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class User_account(models.Model):
     # add_time = models.DateTimeField(default=datetime.now(), verbose_name="添加时间")
 
     user_name = models.CharField(max_length=20, verbose_name='用户名', primary_key=True)
     password = models.CharField(max_length=20, verbose_name="密码")
     # user_signup_time = models.DateTimeField(auto_now_add=time, verbose_name='账号创建时间')
     # user_photo = models.ImageField
+
+
+class Evaluation(models.Model):
+    date = models.DateTimeField(auto_now_add=time, verbose_name='评价生成时间')
+    eva_user = models.CharField(max_length=20, verbose_name='评价用户')
+    eva_comment = models.CharField(max_length=500, verbose_name='评价内容')
+    # eva_course
+
+
+class College(models.Model):
+    college_name = models.CharField
+
+
+class Teacher(models.Model):
+    w = 1
+
+
+class Course(models.Model):
+    w = 1
--- EVA/views.py
+++ EVA/views.py
@@ -1,53 +1,53 @@
 # Create your views here.
 from django.http import HttpResponse
 
-from models import User
+from models import User_account
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the eva index.")
 
 
 def add_user(user_name, password):
-    if (User.objects.filter(user_name=user_name).exists()):
-        return False
+    if (User_account.objects.filter(user_name=user_name).exists()):
+        return 1
     else:
-        user = User.objects.create(name=user_name, password=password)
+        user = User_account.objects.create(name=user_name, password=password)
         user.save()
-        return True
+        return 0
 
 
 def delete_user(user_name):
-    if (User.objects.filter(user_name=user_name).exists()):
-        user = User.objects.get(user_name=user_name)
+    if (User_account.objects.filter(user_name=user_name).exists()):
+        user = User_account.objects.get(user_name=user_name)
         user.delete()
-        return True
+        return 0
     else:
-        return False
+        return 1
 
 
 def get_user_needmodify(user_name):
-    if (User.objects.filter(user_name=user_name).exists()):
-        user = User.objects.get(user_name=user_name)
+    if (User_account.objects.filter(user_name=user_name).exists()):
+        user = User_account.objects.get(user_name=user_name)
         return user
     else:
-        return False
+        return 1
 
 
 def modify_password(user_name, password):
-    if (User.objects.filter(user_name=user_name).exists()):
-        user = User.objects.get(user_name=user_name)
+    if (User_account.objects.filter(user_name=user_name).exists()):
+        user = User_account.objects.get(user_name=user_name)
         user.password = password
-        return True
+        return 0
     else:
-        return False
+        return 1
 
 
 def check_password(user_name, password):
-    user = User.objects.get(user_name=user_name)
+    user = User_account.objects.get(user_name=user_name)
     if (password == user.password):
-        return True
+        return 0
     else:
-        return False
+        return 1
 
 # def check_user()
