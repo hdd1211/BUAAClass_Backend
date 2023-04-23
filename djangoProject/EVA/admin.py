168ab62c6c0f3c03b3093d06d398d7b47399b6b5
“MythSH20” <MythSH20.outlook.com>
2023-04-24 01:39:09 +0800

    alpha_v1.4

---
+++ djangoProject/EVA/admin.py
@@ -0,0 +1,4 @@
+# from .models import User
+
+# admin.site.register(User)
+# Register your models here.
---
+++ djangoProject/EVA/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User_account(models.Model):
+    # add_time = models.DateTimeField(default=datetime.now(), verbose_name="添加时间")
+
+    name = models.CharField(max_length=20, verbose_name='用户名', primary_key=True)
+    password = models.CharField(max_length=20, verbose_name="密码")
+    time = models.DateTimeField(auto_now_add=time, verbose_name='账号创建时间')
+    user_photo = models.ImageField(verbose_name='用户头像')
+
+
+class Evaluation(models.Model):
+    time = models.DateTimeField(auto_now_add=time, verbose_name='评价生成时间')
+    user = models.CharField(max_length=20, verbose_name='评价用户')
+    comment = models.CharField(max_length=500, verbose_name='评价内容')
+    # eva_course
+
+
+class College(models.Model):
+    name = models.CharField
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=20, verbose_name='教师姓名', primary_key=True)
+    courses = models.ManyToManyField(Course)
+    photo = models.ImageField(verbose_name='教师图片')
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=20, verbose_name='教师姓名', primary_key=True)
+    credit = models.FloatField(verbose_name='学分', editable=False, decimal=1)
+    teacher = models.ManyToManyField(Teacher)
+    # 此处不需要再有一个teacher属性？或是teacher中不需要course属性？(MTMF类型会提供反向调用的参数)
+
+    # testtststsdsds
