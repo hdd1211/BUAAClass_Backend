520421419cb8fa0c057ed33929f729ea0f57ace5
“MythSH20” <MythSH20.outlook.com>
2023-04-29 12:59:27 +0800

    alpha_v1.7

--- djangoProject/EVA/models.py
+++ djangoProject/EVA/models.py
@@ -1,27 +1,41 @@
+import datetime
+
 from django.db import models
 
 
 # Create your models here.
 # def class Course
 
-class User_account(models.Model):
-    # add_time = models.DateTimeField(default=datetime.now(), verbose_name="添加时间")
+class User(models.Model):
+    id = models.IntegerField(verbose_name='用户ID', auto_created=True, primary_key=True)
+    name = models.CharField(verbose_name='用户ID', max_length=20, null=False)
+    password = models.CharField(verbose_name="密码", max_length=20, null=False)
+    time = models.DateTimeField(verbose_name='账号创建时间', auto_now_add=datetime.datetime.now())
+    # user_photo = models.ImageField(verbose_name='用户头像')
 
-    name = models.CharField(max_length=20, verbose_name='用户名', primary_key=True)
-    password = models.CharField(max_length=20, verbose_name="密码")
-    # build_time = models.DateTimeField(auto_now_add=add_time, verbose_name='账号创建时间')
-    user_photo = models.ImageField(verbose_name='用户头像')
+
+class Admin(models.Model):
+    id = models.IntegerField(verbose_name='管理员ID', auto_created=True, primary_key=True)
+    username = models.CharField(verbose_name='管理员名', unique=True, null=False)
+    password = models.CharField(verbose_name='管理员密码', null=False)
 
 
 class Evaluation(models.Model):
-    # time = models.DateTimeField(auto_now_add=time, verbose_name='评价生成时间')
-    user = models.CharField(max_length=20, verbose_name='评价用户')
-    comment = models.CharField(max_length=500, verbose_name='评价内容')
+    evaluationid = models.IntegerField(verbose_name='评价ID', primary_key=True, auto_created=True)
+    userid = models.IntegerField(verbose_name='评价用户ID')
+    content = models.CharField(verbose_name='评论内容', max_length=500)
+    likenum = models.IntegerField(verbose_name='赞数量')
+    dislikenum = models.IntegerField(verbose_name='踩数量')
+
+    time = models.DateTimeField(verbose_name='评价生成时间', auto_now_add=datetime.datetime.now())
+
     # eva_course
 
 
 class College(models.Model):
-    name = models.CharField
+    name = models.CharField(verbose_name='学院名')
+    num = models.IntegerField(verbose_name='学院名系号')
+    courses = models.ManyToManyField(Course)
 
 
 #    courses
@@ -31,12 +45,25 @@ class Teacher(models.Model):
     name = models.CharField(max_length=20, verbose_name='教师姓名', primary_key=True)
     # courses = models.ManyToManyField(Course)
     photo = models.ImageField(verbose_name='教师图片')
+    info_url = models.URLField(verbose_name='教师资料链接')
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=20, verbose_name='教师姓名', primary_key=True)
-    credit = models.FloatField(verbose_name='学分', editable=False)
-    teacher = models.ManyToManyField(Teacher)
+    id = models.IntegerField(verbose_name='课程ID', primary_key=True, unique=True, auto_created=True)
+    name = models.CharField(verbose_name='课程名', max_length=20, null=False, unique=True)
+    teacher = models.CharField(verbose_name='教师名', max_length=20, null=False, unique=True)
+    Introduction = models.CharField(verbose_name='教师简介', null=False, unique=True)
+    Semester = models.IntegerField(verbose_name='课程学期', null=False, unique=True)
+    evaluation = models.IntegerField(verbose_name='评分', null=False, unique=True)
+    quality = models.IntegerField(verbose_name='内容评分', null=False, unique=True)
+    workload = models.IntegerField(verbose_name='工作量评分', null=False, unique=True)
+    appraisal = models.IntegerField(verbose_name='考核评分', null=False, unique=True)
+
     # 此处不需要再有一个teacher属性？或是teacher中不需要course属性？(MTMF类型会提供反向调用的参数)
 
-    # testtststsdsds
+
+class report(models.Model):
+    reportid = models.IntegerField(verbose_name='举报ID', primary_key=True)
+    userid = models.IntegerField(verbose_name='用户ID')
+    index = models.indexes()
+    content = models.CharField(verbose_name='反馈内容')
--- djangoProject/EVA/views.py
+++ djangoProject/EVA/views.py
@@ -1,53 +1,82 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-from .models import User_account
+from .models import User
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the eva index.")
 
 
-def add_user(user_name, password):
-    if (User_account.objects.filter(user_name=user_name).exists()):
-        return 1
+# user part:
+def add_user(request):
+    user_name = request.POST.get('user_name')
+    if (User.objects.filter(user_name=user_name).exists()):
+        response_data = {'success': 1, 'message': '账号已存在'}
+        return JsonResponse(response_data)
     else:
-        user = User_account.objects.create(name=user_name, password=password)
+        user = User.objects.create(name=user_name, password=password)
         user.save()
-        return 0
+        response_data = {'success': 0, 'message': '成功注册'}
+        return JsonResponse(response_data)
 
 
-def delete_user(user_name):
-    if (User_account.objects.filter(user_name=user_name).exists()):
-        user = User_account.objects.get(user_name=user_name)
+def delete_user(request):
+    user_name = request.POST.get('user_name')
+    if (User.objects.filter(user_name=user_name).exists()):
+        user = User.objects.get(user_name=user_name)
         user.delete()
-        return 0
+        response_data = {'success': 0, 'message': '成功注销'}
+        return JsonResponse(response_data)
     else:
-        return 1
+        response_data = {'success': 1, 'message': '账号不存在'}
+        return JsonResponse(response_data)
 
 
-def get_user_needmodify(user_name):
-    if (User_account.objects.filter(user_name=user_name).exists()):
-        user = User_account.objects.get(user_name=user_name)
+def get_user_needmodify(request):
+    user_name = request.POST.get('user_name')
+    if (User.objects.filter(user_name=user_name).exists()):
+        user = User.objects.get(user_name=user_name)
+
         return user
     else:
         return 1
 
 
-def modify_password(user_name, password):
-    if (User_account.objects.filter(user_name=user_name).exists()):
-        user = User_account.objects.get(user_name=user_name)
-        user.password = password
-        return 0
+def modify_password(request):
+    user_name = request.POST.get('user_name')
+    new_password = request.POST.get('new_password')
+    if (User.objects.filter(user_name=user_name).exists()):
+        user = User.objects.get(user_name=user_name)
+        user.password = new_password
+        response_data = {'success': 0, 'message': '成功修改密码'}
+        return JsonResponse(response_data)
     else:
-        return 1
+        response_data = {'success': 1, 'message': '未找到用户'}
+        return JsonResponse(response_data)
 
 
-def check_password(user_name, password):
-    user = User_account.objects.get(user_name=user_name)
-    if (password == user.password):
-        return 0
-    else:
-        return 1
+def check_password(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('password')
+        if User.objects.filter(user_name=user_name):
+            if User.objects.filter(password=password):
+                response_data = {'success': 1, 'message': '用户名和密码未匹配成功'}
+                return JsonResponse(response_data)
+            else:
+                response_data = {'success': 0, 'message': '用户名和密码匹配成功'}
+                return JsonResponse(response_data)
+        else:
+            response_data = {'success': 2, 'message': '用户未注册'}
+            return JsonResponse(response_data)
+
 
 # def check_user()
+
+
+# college:
+
+# Course:
+def add_course(request):
+    name = request.POST.get('course_name')
